03304a60367f9d786d0daee0af59e307340a8fe8
Kilian Prechtl <prekic19@htl-kaindorf.at>
2023-11-28 15:44:38 +0100

    Added get all Subjects from Tutor

--- src/main/java/teamup4success/repository/TutorRepository.java
+++ src/main/java/teamup4success/repository/TutorRepository.java
@@ -1,11 +1,20 @@
 package teamup4success.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import teamup4success.pojos.Subject;
 import teamup4success.pojos.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
 
     Tutor findById(long id);
+
+    @Query("SELECT s FROM Subject s WHERE s.tutor = :tutor")
+    List<Subject> findAllSubjectsByTutor(Tutor tutor);
+
 }
